86e8240a7b035713e396405299af0a2555846e22
Tobias Davis <saibotsivad@Tobiass-MacBook-Air.local>
2013-12-05 23:37:00 -0600

    yeahimanidiot

--- site/index.php
+++ site/index.php
@@ -10,7 +10,7 @@
 // up one folder is the default configs file, but if you use the suggested layout you can have your cake and eat it too!
 if (file_exists('../../../smartstrap-configs.php')) {
 	require('../../../smartstrap-configs.php');
-if (file_exists('../../smartstrap-configs.php')) {
+} else if (file_exists('../../smartstrap-configs.php')) {
 	require('../../smartstrap-configs.php');
 } else {
 	require('../smartstrap-configs.php');
